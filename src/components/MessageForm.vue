759fa482f3206ab006820c72816c714c230f976a
luchenwei9266 <lugege9266@gmail.com>
2019-12-10 10:17:01 +0800

    first blood

---
+++ src/components/MessageForm.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="message-form ld-over">
+    <small class="text-muted">@{{ user.username }}</small>
+    <b-form @submit.prevent="onSubmit" class="ld-over" v-bind:class="{ running: sending }">
+      <div class="ld ld-ring ld-spin"></div>
+      <b-alert variant="danger" :show="hasError">{{ error }} </b-alert>
+      <b-form-group>
+        <b-form-input id="message-input"
+                      type="text"
+                      v-model="message"
+                      @input="isTyping"
+                      placeholder="Enter Message"
+                      autocomplete="off"
+                      required>
+        </b-form-input>
+      </b-form-group>
+      <div class="clearfix">
+        <b-button type="submit" variant="primary" class="float-right">
+          Send
+        </b-button>
+      </div>
+    </b-form>
+  </div>
+</template>
+
+<script>
+import { mapState,mapGetters,mapActions } from 'vuex'
+import { isTyping } from '../chatkit.js';
+
+export default {
+    name: 'message-form',
+    data() {
+        return {
+            message: ''
+        }
+    },
+    computed: {
+        ...mapState([
+            'user',
+            'sending',
+            'error',
+            'activeRoom'
+        ]),
+        ...mapGetters([
+            'hasError'
+        ])
+    },
+    methods: {
+      ...mapActions([
+        'sendMessage'
+      ]),
+      async onSubmit() {
+        const result = await this.sendMessage(this.message);
+        if (result) {
+          this.message = '';
+        }
+      },
+      async isTyping() {
+        await isTyping(this.activeRoom.id);
+      }
+    }
+    
+}
+</script>
---
+++ src/store/actions.js
@@ -0,0 +1,65 @@
+import chatkit from '../chatkit';
+
+function handleError(commit,error) {
+    const message = error.message || error.info.error_description;
+    commit('setError',message);
+}
+
+export default {
+    async login({commit,state},userId) {
+        try {
+            commit('setError','');
+            commit('setLoading',true);
+            const currentUser = await chatkit.connectUser(userId)
+            commit('setUser',{
+                username: currentUser.id,
+                name: currentUser.name
+            });
+            commit('setReconnect',false);
+            console.log(state.user);
+
+            const rooms = currentUser.rooms.map(room => ({
+                id: room.id,
+                name: room.name
+            }));
+            commit('setRooms',rooms);
+
+            const activeRoom = state.activeRoom || rooms[0];
+            commit('setActiveRoom',{
+                id: activeRoom.id,
+                name: activeRoom.name
+            });
+            await chatkit.subscribeToRoom(activeRoom.id);
+            return true;
+        } catch (error) {
+            handleError(commit,error);
+        } finally {
+            commit('setLoading',false)
+        }
+    },
+    async changeRoom({ commit },roomId) {
+        try {
+            const { id,name } = await chatkit.subscribeToRoom(roomId);
+            commit('setActiveRoom',{id,name});
+        } catch (error) {
+            handleError(commit,error)
+        }
+    },
+    async sendMessage({ commit }, message) {
+        try {
+            commit('setError','');
+            commit('setSending',true);
+            const messageId = await chatkit.sendMessage(message);
+            return messageId;
+        } catch(error) {
+            handleError(commit,error);
+        } finally {
+            commit('setSending',false);
+        }
+    },
+    async logout({ commit }) {
+        commit('reset');
+        chatkit.disconnectUser();
+        window.localStorage.clear();
+    }
+}
